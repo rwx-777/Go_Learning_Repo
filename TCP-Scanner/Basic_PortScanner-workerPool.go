a612a966594ac646d4a53f677e567625475ba5d5
rwx777 <joshuajared007@gmail.com>
2020-07-27 11:30:56 +0200

    Add files via upload

---
+++ TCP-Scanner/Basic_PortScanner-workerPool.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(ports chan int, wg *sync.WaitGroup) {
+	for p := range ports {
+		fmt.Println(p)
+		wg.Done()
+	}
+}
+
+func main() {
+	ports := make(chan int, 100)
+	var wg sync.WaitGroup
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, &wg)
+	}
+	for i := 1; i <= 1024; i++ {
+
+	}
+}
